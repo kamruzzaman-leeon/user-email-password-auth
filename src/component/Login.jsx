c3c34824d04cd182c619cad8b557640c25b61939
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2023-10-13 23:42:45 +0600

    Login User and Accept terms and conditions

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,12 +1,63 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { Helmet } from "react-helmet-async";
+import auth from "../firebase.config";
 
 const Login = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email,password)
+        signInWithEmailAndPassword(auth,email,password)
+
+        .then(result => {
+            console.log(result.user);
+           
+        })
+        .catch(error => {
+            console.error(error);
+            
+        })
+    }
     return (
         <div>
             <Helmet>
                 <title>Login</title>
             </Helmet>
-           <h2 className='text-3xl'>Please Login</h2>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Login now!</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit = {handleSubmit}className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input 
+                                name="email"
+                                type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input 
+                                name="password"
+                                type="password" placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button name="Submit" className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/component/Register.jsx
+++ src/component/Register.jsx
@@ -13,16 +13,21 @@ const Register = () => {
         // console.log('form submitting')
         const email = e.target.email.value;
         const password = e.target.password.value;
-        // console.log(email, password);
+        const accepted = e.target.terms.value;
+        console.log(email, password,accepted);
 
         if(password.length < 6){
             setRegError('Password should be at least 6 characters ');
             return;
         }
-        else if(!/A-Z/.test(password)){
+        else if(/A-Z/.test(password)){
             setRegError('Your password should have at least one upper case characters!');
             return;
         }
+           else if(!accepted){
+            setRegError('please accept the terms & condition of ours!')
+            return;
+           }
 
         setRegError('');
         setRegSuccess('');
@@ -44,20 +49,24 @@ const Register = () => {
                 <h2 className='text-3xl text-center py-4 mb-8'>Registration</h2>
                 <form onSubmit={handleRegister} className='items-center'>
                     <input className='mb-4 w-3/4 py-2 px-4' type="email" name="email" id="" placeholder='Enter Email' required/>
-                    <div className='input-group mb-4 w-3/4 '>
+                    <div className='relative mb-4 w-3/4 '>
                     <input
                     type={showPassword?"text":"password"}
-                    className='py-2 px-4'
+                    className='py-2 px-4 w-full'
                      name="password"
                      id="" 
                      placeholder='Enter Password' required/>
-                    <span onClick={()=> setShowPassword(!showPassword)}>
+                    <span className='absolute top-3 right-4
+                    ' onClick={()=> setShowPassword(!showPassword)}>
                         {
                             showPassword ?<FaRegEye></FaRegEye>:<FaRegEyeSlash></FaRegEyeSlash>
                         }
                     </span>
                     </div>
-                    
+                    <div className='mb-1'>
+                    <input type="checkbox" name="terms" id="terms" />
+                    <label htmlFor="terms"> Accept our  terms & conditions</label>
+                    </div>
                     <input className='mb-4 w-3/4 btn btn-primary py-2 px-4' type="submit" value="Register" />
 
                 </form>
